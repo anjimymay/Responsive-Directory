5da9f97c8b07af796bc879e6deb94657063b029a
jaimepreuss <jaimepreuss@gmail.com>
2020-05-28 01:23:56 -0700

    updated all html doc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-
+//function for sticky navbar
 function myFunction() {
  var stick = window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+myFunction();
